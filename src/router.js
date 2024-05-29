773905e88f5c2415577f13b5616f205d8a0b21ee
omarperez <omar.p@reflexmediagroup.com>
2024-05-29 08:14:03 +0200

    Github deploy fixed

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Layout from './components/Layout.vue';
 import HomeView from './views/HomeView.vue';
 import CustomerView from './views/CustomerView.vue';
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
